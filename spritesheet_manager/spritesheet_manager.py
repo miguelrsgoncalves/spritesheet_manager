bff2aee5eb3dedd47a916fd43f022a42da026494
miguelrsgoncalves <miguelrsgoncalves@miguelrsgoncalves.com>
2026-04-09 15:26:14 +0100

    Refactor spritesheet_manager.py

---
+++ spritesheet_manager/spritesheet_manager.py
@@ -0,0 +1,24 @@
+from krita import Extension
+from PyQt5.QtWidgets import QMenu
+from .atlas_editor.actions import create_atlas_editor_actions
+from .spritesheet_editor.actions import create_spritesheet_editor_actions
+
+class SpritesheetManagerExtension(Extension):
+    def __init__(self, parent):
+        super().__init__(parent)
+
+    def setup(self):
+        pass
+
+    def createActions(self, window):
+        # Spritesheet Manager menu
+        spritesheet_manager_menu = QMenu("Spritesheet Manager", window)
+        window.menuBar().addMenu(spritesheet_manager_menu)
+
+        # Atlas Editor submenu
+        atlas_editor_submenu = spritesheet_manager_menu.addMenu("Atlas Editor")
+        create_atlas_editor_actions(self, window, atlas_editor_submenu)
+
+        # Spritesheet Editor submenu
+        spritesheet_editor_submenu = spritesheet_manager_menu.addMenu("Spritesheet Editor")
+        create_spritesheet_editor_actions(self, window, spritesheet_editor_submenu)
