b8dfcb558ab3794be513398209c07ca84981b53b
miguelrsgoncalves <miguelrsgoncalves@miguelrsgoncalves.com>
2026-04-10 15:43:50 +0100

    Add Serializer class

--- spritesheet_manager/core/serializer.py
+++ spritesheet_manager/core/serializer.py
@@ -5,36 +5,37 @@ import json
 
 PLUGIN_KEY: str = "SPRITESHEET_MANAGER"
 
-def load_setting(key: str, default_value: str = "") -> str:
-    return Krita.instance().readSetting(PLUGIN_KEY, key, str(default_value))
+class Serializer:
+    def load_setting(key: str, default_value: str = "") -> str:
+        return Krita.instance().readSetting(PLUGIN_KEY, key, str(default_value))
 
-def save_setting(key: str, value: str):
-    Krita.instance().writeSetting(PLUGIN_KEY, key, str(value))
+    def save_setting(key: str, value: str):
+        Krita.instance().writeSetting(PLUGIN_KEY, key, str(value))
 
-def load_state(document, key: str) -> dict:
-    if not document: return {}
+    def load_state(document, key: str) -> dict:
+        if not document: return {}
 
-    state_key: str = PLUGIN_KEY + "_" + key
-    
-    annotation: str = document.annotation(state_key)
+        state_key: str = PLUGIN_KEY + "_" + key
+        
+        annotation: str = document.annotation(state_key)
 
-    if not annotation or len(annotation) == 0: return {}
-    
-    try:
-        data: str = bytes(annotation).decode("utf-8")
-        return json.loads(data)
-    except Exception:
-        return {}
+        if not annotation or len(annotation) == 0: return {}
+        
+        try:
+            data: str = bytes(annotation).decode("utf-8")
+            return json.loads(data)
+        except Exception:
+            return {}
 
-def save_state(document, key: str, data: dict, description: str = ""):
-    if not document: return
+    def save_state(document, key: str, data: dict, description: str = ""):
+        if not document: return
 
-    data["_version"] = PLUGIN_VERSION
-    
-    state_key: str = PLUGIN_KEY + "_" + key
+        data["_version"] = PLUGIN_VERSION
+        
+        state_key: str = PLUGIN_KEY + "_" + key
 
-    try:
-        data: bytes = json.dumps(data).encode("utf-8")
-        document.setAnnotation(state_key, description, QByteArray(data))
-    except Exception:
-        pass
+        try:
+            data: bytes = json.dumps(data).encode("utf-8")
+            document.setAnnotation(state_key, description, QByteArray(data))
+        except Exception:
+            pass
