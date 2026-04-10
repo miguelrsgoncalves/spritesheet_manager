b76d15605ccf8f1ddfa2c28022409c8b1f15f77a
miguelrsgoncalves <miguelrsgoncalves@miguelrsgoncalves.com>
2026-04-10 16:35:16 +0100

    Add staticmethod to Serializer functions

--- spritesheet_manager/core/serializer.py
+++ spritesheet_manager/core/serializer.py
@@ -6,12 +6,15 @@ import json
 PLUGIN_KEY: str = "SPRITESHEET_MANAGER"
 
 class Serializer:
+    @staticmethod
     def load_setting(key: str, default_value: str = "") -> str:
         return Krita.instance().readSetting(PLUGIN_KEY, key, str(default_value))
 
+    @staticmethod
     def save_setting(key: str, value: str):
         Krita.instance().writeSetting(PLUGIN_KEY, key, str(value))
 
+    @staticmethod
     def load_state(document, key: str) -> dict:
         if not document: return {}
 
@@ -22,11 +25,12 @@ class Serializer:
         if not annotation or len(annotation) == 0: return {}
         
         try:
-            data: str = bytes(annotation).decode("utf-8")
-            return json.loads(data)
+            raw_data: str = bytes(annotation).decode("utf-8")
+            return json.loads(raw_data)
         except Exception:
             return {}
 
+    @staticmethod
     def save_state(document, key: str, data: dict, description: str = ""):
         if not document: return
 
@@ -35,7 +39,7 @@ class Serializer:
         state_key: str = PLUGIN_KEY + "_" + key
 
         try:
-            data: bytes = json.dumps(data).encode("utf-8")
-            document.setAnnotation(state_key, description, QByteArray(data))
+            raw_data: bytes = json.dumps(data).encode("utf-8")
+            document.setAnnotation(state_key, description, QByteArray(raw_data))
         except Exception:
             pass
