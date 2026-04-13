05a35ceb6614a9f76b5776419b01356ce991267b
miguelrsgoncalves <miguelrsgoncalves@miguelrsgoncalves.com>
2026-04-14 00:18:03 +0100

    Refactor Padder

--- spritesheet_manager/core/padder.py
+++ spritesheet_manager/core/padder.py
@@ -1,7 +1,5 @@
 import os
 from krita import InfoObject
-from PyQt5.QtWidgets import QFileDialog
-from .pixel import read_pixels, write_pixels, repeat_row_vertically
 
 EXPORT_FILTERS = (
     "PNG Image (*.png);;"
@@ -50,7 +48,7 @@ class Padder():
             self._document.resolution()
         )
 
-        root_layer = padded_document.root_layerNode()
+        root_layer = padded_document.rootNode()
         for child in root_layer.childNodes():
             root_layer.removeChildNode(child)
 
@@ -65,20 +63,19 @@ class Padder():
                 destination_y: int = (row * stride_height) + self._padding_size[1]
 
                 tile_data = self._document.pixelData(
-                    self._document,
                     source_x, source_y,
                     self._tile_size[0], self._tile_size[1]
                 
                 )
+
                 padded_layer.setPixelData(
-                    padded_layer,
                     tile_data,
                     destination_x, destination_y,
                     self._tile_size[0], self._tile_size[1]
                 )
 
                 if self._is_anti_bleed and (self._padding_size[0] > 0 or self._padding_size[1] > 0):
-                    self.anti_bleed(
+                    self.apply_anti_bleed(
                         self._document,
                         padded_layer,
                         source_x, source_y,
@@ -109,33 +106,38 @@ class Padder():
 
         return padded_document
     
-    def anti_bleed(self, source_doc, dest_layer, source_x, source_y, dest_x, dest_y,
-                        tile_size[0], tile_size[1], padding_size[0], padding_size[1]):
+    #region anti_bleed
 
-        # Top and bottom edge bands
-        if padding_size[1] > 0:
-            top_row = read_pixels(source_doc, source_x, source_y, tile_size[0], 1)
-            bottom_row = read_pixels(source_doc, source_x, source_y + tile_size[1] - 1, tile_size[0], 1)
-            write_pixels(dest_layer, repeat_row_vertically(top_row, padding_size[1]), dest_x, dest_y - padding_size[1], tile_size[0], padding_size[1])
-            write_pixels(dest_layer, repeat_row_vertically(bottom_row, padding_size[1]), dest_x, dest_y + tile_size[1], tile_size[0], padding_size[1])
+    def apply_anti_bleed(self, source_document, destination_layer, source_x, source_y, destination_x, destination_y):
+        if self._padding_size[0] > 0:
+            left_column = source_document.pixelData(source_x, source_y, 1, self._tile_size[1])
+            right_column = source_document.pixelData(source_x + self._tile_size[0] - 1, source_y, 1, self._tile_size[1])
 
-        # Left and right edge bands
-        if padding_size[0] > 0:
-            left_col = read_pixels(source_doc, source_x, source_y, 1, tile_size[1])
-            right_col = read_pixels(source_doc, source_x + tile_size[0] - 1, source_y, 1, tile_size[1])
-            for i in range(1, padding_size[0] + 1):
-                write_pixels(dest_layer, left_col, dest_x - i, dest_y, 1, tile_size[1])
-                write_pixels(dest_layer, right_col, dest_x + tile_size[0] + i - 1, dest_y, 1, tile_size[1])
+            for i in range(1, self._padding_size[0] + 1):
+                destination_layer.setPixelData(left_column, destination_x - i, destination_y, 1, self._tile_size[1])
+                destination_layer.setPixelData(right_column, destination_x + self._tile_size[0] + i - 1, destination_y, 1, self._tile_size[1])
 
-        # Corner blocks filled with the single nearest corner pixel repeated
-        if padding_size[0] > 0 and padding_size[1] > 0:
+        if self._padding_size[1] > 0:
+            top_row = source_document.pixelData(source_x, source_y, self._tile_size[0], 1)
+            bottom_row = source_document.pixelData(source_x, source_y + self._tile_size[1] - 1, self._tile_size[0], 1)
+
+            for i in range(1, self._padding_size[1] + 1):
+                destination_layer.setPixelData(top_row, destination_x, destination_y - i, self._tile_size[0], 1)
+                destination_layer.setPixelData(bottom_row, destination_x, destination_y + self._tile_size[1] + i - 1, self._tile_size[0], 1)
+
+        if self._padding_size[0] > 0 and self._padding_size[1] > 0:
             corners = [
-                (source_x, source_y, dest_x - padding_size[0], dest_y - padding_size[1]),
-                (source_x + tile_size[0] - 1, source_y, dest_x + tile_size[0], dest_y - padding_size[1]),
-                (source_x, source_y + tile_size[1] - 1, dest_x - padding_size[0], dest_y + tile_size[1]),
-                (source_x + tile_size[0] - 1, source_y + tile_size[1] - 1, dest_x + tile_size[0], dest_y + tile_size[1]),
+                (source_x, source_y, destination_x - self._padding_size[0], destination_y - self._padding_size[1]),
+                (source_x + self._tile_size[0] - 1, source_y, destination_x + self._tile_size[0], destination_y - self._padding_size[1]),
+                (source_x, source_y + self._tile_size[1] - 1, destination_x - self._padding_size[0], destination_y + self._tile_size[1]),
+                (source_x + self._tile_size[0] - 1, source_y + self._tile_size[1] - 1, destination_x + self._tile_size[0], destination_y + self._tile_size[1]),
             ]
 
             for corner_source_x, corner_source_y, corner_dest_x, corner_dest_y in corners:
-                pixel = read_pixels(source_doc, corner_source_x, corner_source_y, 1, 1)
-                write_pixels(dest_layer, pixel * (padding_size[0] * padding_size[1]), corner_dest_x, corner_dest_y, padding_size[0], padding_size[1])
+                pixel = source_document.pixelData(corner_source_x, corner_source_y, 1, 1)
+                
+                corner_block = pixel * (self._padding_size[0] * self._padding_size[1])
+                
+                destination_layer.setPixelData(corner_block, corner_dest_x, corner_dest_y, self._padding_size[0], self._padding_size[1])
+    
+    #endregion
