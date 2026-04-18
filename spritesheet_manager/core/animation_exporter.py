f7cd6568f8e7a2e9252c727bef26dee0e8cb8277
miguelrsgoncalves <miguelrsgoncalves@miguelrsgoncalves.com>
2026-04-18 14:30:38 +0100

    Add AnimationExporter init arguments

--- spritesheet_manager/core/animation_exporter.py
+++ spritesheet_manager/core/animation_exporter.py
@@ -1,8 +1,39 @@
-from krita import Krita
+import os
+from krita import Krita, InfoObject
+from enum import Enum
+
+PREVIEW_RESOLUTION_SCALING = 0.15
 
 class AnimationExporter:
-    def __init__(self, document):
+    class PackingType(Enum):
+        HORIZONTAL = 1
+        VERTICAL = 2
+        SQUARE = 3
+    
+
+    def __init__(
+            self,
+            document,
+            start_frame: int,
+            end_frame: int,
+            frame_step: int,
+            columns: int,
+            rows: int,
+            packing_type: PackingType,
+            export_name: str,
+            is_export_kra: bool,
+            is_export_image: bool,
+    ):
         self._document = document
+        self._start_frame = start_frame
+        self._end_frame = end_frame
+        self._frame_step = frame_step
+        self._columns = columns
+        self._rows = rows
+        self._packing_type = packing_type
+        self._export_name = export_name
+        self._is_export_kra = is_export_kra
+        self._is_export_image = is_export_image
 
     def run(self):
         if not self._document: return
