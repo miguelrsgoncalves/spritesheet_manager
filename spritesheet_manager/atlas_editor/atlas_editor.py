7bcd9c48ff3e785f3ff44a0881181ee4c4e1297e
miguelrsgoncalves <miguelrsgoncalves@miguelrsgoncalves.com>
2026-04-18 13:01:20 +0100

    Add warning message when Atlas Editor tools get called with no active document

--- spritesheet_manager/atlas_editor/atlas_editor.py
+++ spritesheet_manager/atlas_editor/atlas_editor.py
@@ -1,4 +1,5 @@
 from krita import Krita
+from PyQt5.QtWidgets import QMainWindow, QAction, QMessageBox
 from .models.atlas_model import AtlasModel
 from .states.atlas_state import load_atlas_state, save_atlas_state
 
@@ -34,4 +35,15 @@ class AtlasEditor:
         self.save_to_document(doc)
 
 def has_active_document() -> bool:
-    return True if Krita.instance().activeDocument() else False
+    document = Krita.instance().activeDocument()
+    if document is not None: return True
+    
+    warning_message: QMessageBox = QMessageBox()
+    warning_message.setIcon(QMessageBox.Warning)
+    warning_message.setText("No Active Document Found!")
+    warning_message.setInformativeText("You need to have a document open to use the Spritesheet Manager tools!")
+    warning_message.setWindowTitle("Spritesheet Manager")
+    warning_message.setStandardButtons(QMessageBox.Ok)
+    warning_message.exec_()
+    
+    return False
