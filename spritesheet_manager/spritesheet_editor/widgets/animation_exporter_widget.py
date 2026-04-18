23e5266a36c0c1ac00d190d87c3021ee88b8e4c6
miguelrsgoncalves <miguelrsgoncalves@miguelrsgoncalves.com>
2026-04-18 14:33:41 +0100

    Add imports and global vars to AnimationExporterWidget

--- spritesheet_manager/spritesheet_editor/widgets/animation_exporter_widget.py
+++ spritesheet_manager/spritesheet_editor/widgets/animation_exporter_widget.py
@@ -1,6 +1,24 @@
 from krita import Krita
-from PyQt5.QtWidgets import QWidget, QDialog, QVBoxLayout, QDialogButtonBox
+from PyQt5.QtCore import Qt, QTimer
+from PyQt5.QtGui import QPixmap, QImage
+from PyQt5.QtWidgets import QWidget, QDialog, QGroupBox, QGridLayout, QHBoxLayout, QVBoxLayout, QLabel, QSpinBox, QCheckBox, QLineEdit, QDialogButtonBox
+from ...core.serializer import Serializer
 from ...core.animation_exporter import AnimationExporter
+from ...ui.widgets import LinkButton
+
+MAX_INT = 2147483647
+PREVIEW_TIMER_INTERVAL = 1000
+PREVIEW_ASPECT_RATIO = 16 / 9
+PREVIEW_WINDOW_SIZE = [480, 270, 640, 360]
+
+WIDGET_KEY: str = "ANIMATION_EXPORTER"
+WIDGET_DESCRIPTION: str = "Animation Exporter settings"
+
+DEFAULTS: dict[str, any] = {
+    "is_export_kra": False,
+    "is_export_image": True,
+    "animation_file_suffix": "_animation"
+}
 
 class AnimationExporterWidget(QWidget):
     def __init__(self, document):
