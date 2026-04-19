e67bf5a8ef15fca2aa31e969a9712b0099f2eb82
miguelrsgoncalves <miguelrsgoncalves@miguelrsgoncalves.com>
2026-04-19 01:27:31 +0100

    Add preview to AnimationExportWidget

--- spritesheet_manager/spritesheet_editor/widgets/animation_exporter_widget.py
+++ spritesheet_manager/spritesheet_editor/widgets/animation_exporter_widget.py
@@ -24,6 +24,21 @@ class AnimationExporterWidget(QWidget):
     def __init__(self, document):
         super().__init__()
         self._document = document
+
+        self._preview_timer: QTimer = QTimer()
+        self._preview_timer.setSingleShot(True)
+        self._preview_timer.timeout.connect(self._update_preview)
+
+        layout: QVBoxLayout = QVBoxLayout()
+
+        layout.addWidget(self._build_preview_group())
+        layout.addWidget(self._build_animation_settings_group())
+        layout.addWidget(self._build_options_group())
+        layout.addWidget(self._build_output_group())
+
+        self.setLayout(layout)
+
+        self.refresh_ui()
     
     def run_animation_exporter(self):
         animation_exporter: AnimationExporter = AnimationExporter(**self._get_animation_exporter_arguments())
@@ -42,6 +57,24 @@ class AnimationExporterWidget(QWidget):
         self._on_grid_auto_update_toggled()
         self._on_padding_auto_update_toggled()
     
+    def _update_preview(self):
+        animation_exporter_arguments: dict[str, any] = self._get_animation_exporter_arguments()
+        padder: AnimationExporter = AnimationExporter(**animation_exporter_arguments)
+        preview_document, final_width, final_height = padder.run(True)
+
+        if preview_document:
+            q_image: QImage = preview_document.thumbnail(PREVIEW_WINDOW_SIZE[0], PREVIEW_WINDOW_SIZE[1])
+            preview_document.close()
+
+            self._preview_window.setPixmap(QPixmap.fromImage(q_image))
+            self._preview_label.setText(f"Export Resolution: {final_width}x{final_height} px")
+
+    
+    def _get_default_animation_export_name(self) -> str:
+        return self._document.name() + DEFAULTS.get("padded_file_suffix") if self._document else "Animation Spritesheet"
+    
+    #endregion
+
 
 class AnimationExporterDialog(QDialog):
     def __init__(self):
