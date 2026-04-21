19305f1b563c6242166204f93ebdf15e20b62995
miguelrsgoncalves <miguelrsgoncalves@miguelrsgoncalves.com>
2026-04-21 15:28:25 +0100

    Finish PreviewWindow

--- spritesheet_manager/core/widgets.py
+++ spritesheet_manager/core/widgets.py
@@ -37,19 +37,25 @@ class LinkButton(QToolButton):
 class PreviewWindow(QWidget):
     def __init__(
             self,
-            timer_interval = 1000,
-            timer_tick_interval = 10,
-            timer_tick_label = "Refreshing preview in {:.3f} s",
-            aspect_ratio = 16 / 9,
-            window_size = [480, 270, 640, 360],
+            refresh_callback: callable,
+            timer_interval: int = 1000,
+            timer_tick_interval: int = 10,
+            timer_tick_label: str = "Refreshing preview in {:.3f} s",
+            aspect_ratio: int = 16 / 9,
+            window_size: list[int] = [480, 270, 640, 360],
     ):
         super().__init__()
         
-        self._timer_interval = timer_interval
-        self._timer_tick_interval = timer_tick_interval
-        self._timer_tick_label = timer_tick_label
-        self._aspect_ration = aspect_ratio
-        self._window_size = window_size
+        self._refresh_callback: callable = refresh_callback
+        self._timer_interval: int = timer_interval
+        self._timer_tick_interval: int = timer_tick_interval
+        self._timer_tick_label: str = timer_tick_label
+        self._aspect_ration: int = aspect_ratio
+        self._window_size: list[int] = window_size
+
+        self._preview_timer: QTimer = QTimer()
+        self._preview_timer_interval: int = 0
+        self._preview_timer.timeout.connect(self._on_timer_tick)
 
         preview_layout: QVBoxLayout = QVBoxLayout(self)
         preview_layout.setAlignment(Qt.AlignmentFlag.AlignCenter)
@@ -58,10 +64,10 @@ class PreviewWindow(QWidget):
 
         self._auto_update_preview_checkbox: QCheckBox = QCheckBox("Auto-update Preview")
         self._auto_update_preview_checkbox.setChecked(True)
-        self._auto_update_preview_checkbox.toggled.connect(self._on_animation_exporter_argument_changed)
+        self._auto_update_preview_checkbox.toggled.connect(self.request_refresh)
 
         self._preview_manual_update_button: QPushButton = QPushButton("Refresh")
-        self._preview_manual_update_button.clicked.connect(self._update_preview)
+        self._preview_manual_update_button.clicked.connect(self.force_refresh)
 
         preview_controls_layout.addWidget(self._auto_update_preview_checkbox)
         preview_controls_layout.addWidget(self._preview_manual_update_button)
@@ -90,21 +96,31 @@ class PreviewWindow(QWidget):
         preview_layout.addWidget(self._preview_window)
         preview_layout.addWidget(self._preview_resolution_label)
         
-        self.addLayout(preview_layout)
+        self.setLayout(preview_layout)
     
-    def _refresh(self, algorithm: any, arguments: any):
+    def request_refresh(self):
+        if self._auto_update_preview_checkbox.isChecked():
+            self._preview_timer_interval = self._timer_interval
+            self._preview_timer.start(self._timer_tick_interval)
+        else:
+            self._preview_timer.stop()
+            self._preview_window.setText("Preview out of date")
+    
+    def force_refresh(self):
+        self._preview_timer.stop()
+        self._refresh()
+    
+    def _refresh(self):
+        if not self._refresh_callback: return
+
         self._preview_window.clear()
-        self._preview_window.setText("Rendering preview...")
+        self._preview_window.setText("Rendering...")
 
-        exporter = algorithm(**arguments)
-        preview_document, final_width, final_height = exporter.run(True)
+        q_image, width, height = self._refresh_callback()
 
-        if preview_document:
-            q_image: QImage = preview_document.thumbnail(self._window_size[0], self._window_size[1])
-            preview_document.close()
-
+        if q_image:
             self._preview_window.setPixmap(QPixmap.fromImage(q_image))
-            self._preview_resolution_label.setText(f"Export Resolution: {final_width}x{final_height} px")
+            self._preview_resolution_label.setText(f"Export Resolution: {width}x{height} px")
 
     def _on_timer_tick(self):
         self._preview_timer_interval -= self._timer_tick_interval
@@ -114,4 +130,4 @@ class PreviewWindow(QWidget):
             self._preview_window.setText(self._timer_tick_label.format(time))
         else:
             self._preview_timer.stop()
-            self._update_preview()
+            self._refresh()
