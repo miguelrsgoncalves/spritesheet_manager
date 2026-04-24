5c590043f81e27aa5dfcf2c41235a40d21f67291
miguelrsgoncalves <miguelrsgoncalves@miguelrsgoncalves.com>
2026-04-25 00:05:31 +0100

    Update AtlasEditorWidget

--- spritesheet_manager/atlas_editor/atlas_editor.py
+++ spritesheet_manager/atlas_editor/atlas_editor.py
@@ -1,31 +1,31 @@
 from krita import Krita, DockWidgetFactory, DockWidgetFactoryBase
 from PyQt5.QtWidgets import QMainWindow, QAction
 from ..core.widgets import ActiveDocumentWarningMessage
-from .widgets.atlas_editor_widget import AtlasEditorDock
+from .widgets.atlas_editor_widget import AtlasEditorDocker
 
 def create_atlas_editor_actions(plugin_instance, window, menu):
     main_window: QMainWindow = window.qwindow()
 
     setup_dockers()
-    
-    atlas_editor_dock_action: QAction = QAction("Open Dock", main_window)
-    atlas_editor_dock_action.triggered.connect(lambda: run_atlas_editor_dock(main_window))
-    atlas_editor_dock_action.setToolTip("Open Atlas Editor dock.")
-    menu.addAction(atlas_editor_dock_action)
+
+    atlas_editor_docker_action: QAction = QAction("Open Docker", main_window)
+    atlas_editor_docker_action.triggered.connect(lambda: run_atlas_editor_docker(main_window))
+    atlas_editor_docker_action.setToolTip("Open Atlas Editor docker.")
+    menu.addAction(atlas_editor_docker_action)
 
 def setup_dockers():
     atlas_editor_docker_factory = DockWidgetFactory(
-        AtlasEditorDock.DOCKER_KEY, 
-        DockWidgetFactoryBase.DockRight, 
-        AtlasEditorDock
+        AtlasEditorDocker.DOCKER_KEY,
+        DockWidgetFactoryBase.DockRight,
+        AtlasEditorDocker
     )
     
     Krita.instance().addDockWidgetFactory(atlas_editor_docker_factory)
 
-def run_atlas_editor_dock(main_window):    
+def run_atlas_editor_docker(main_window):    
     dockers = main_window.dockers()
     for docker in dockers:
-        if docker.objectName() == AtlasEditorDock.DOCKER_KEY:
+        if docker.objectName() == AtlasEditorDocker.DOCKER_KEY:
             docker.setVisible(True)
             docker.raise_()
             return
--- spritesheet_manager/atlas_editor/widgets/atlas_editor_widget.py
+++ spritesheet_manager/atlas_editor/widgets/atlas_editor_widget.py
@@ -1,8 +1,41 @@
-WIDGET_KEY: str = "ATLAS_EDITOR"
+from krita import Krita, DockWidget
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QWidget, QDialog, QGroupBox, QGridLayout, QHBoxLayout, QVBoxLayout, QLabel, QSpinBox, QCheckBox, QLineEdit, QDialogButtonBox
+from ...core.serializer import Serializer
 
 class AtlasEditorWidget:
-    pass
+    WIDGET_KEY: str = "ATLAS_EDITOR"
+    
+    def __init__(self, parent, document):
+        super().__init__(parent)
 
-class AtlasEditorDock:
-    DOCKER_KEY: str = "ATLAS_EDITOR_DOCK"
-    pass
+        self._layout: QVBoxLayout = QVBoxLayout()
+        
+        self._document_label: QLabel = QLabel("No Active Document")
+        self._document_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        
+        self._layout.addWidget(self._document_label)
+        self.setLayout(self._layout)
+        
+        self.refresh_ui()
+    
+    def refresh_ui(self):
+        document = Krita.instance().activeDocument()
+        if document:
+            self._document_label.setText(f"Active Document: {document.name()}")
+        else:
+            self._document_label.setText("No Active Document")
+
+class AtlasEditorDocker(DockWidget):
+    DOCKER_KEY: str = "ATLAS_EDITOR_DOCKER"
+    
+    def __init__(self):
+        super().__init__()
+        
+        self.setWindowTitle("Atlas Editor")
+        
+        self._widget: AtlasEditorWidget = AtlasEditorWidget(self)
+        self.setWidget(self._widget)
+
+    def canvasChanged(self, canvas):
+        self._widget.refresh_ui()
